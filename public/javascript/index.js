b97a26411494759c5115808bb6e7e396f1691521
Marianne Agulhon <marianne.agulhon@gmail.com>
2023-02-17 14:27:34 +0100

    Fetch all characters done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,26 +1,64 @@
-  /**
-   * You might want to use this template to display each new characters
-   * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
-   */
-  const characterTemplate = document.getElementById('template')
+/**
+ * You might want to use this template to display each new characters
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
+ */
+const characterTemplate = document.getElementById("template");
+const displayAllCharacters = document.querySelector(".characters-container");
 
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+/**
+ * Create an instance of axios
+ */
+const characterApi = axios.create({
+  baseURL: "http://localhost:5005/api/characters/",
+});
 
-  });
+const myUrl = "http://localhost:5005/api/";
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+const fetchAllBtn = document.getElementById("fetch-all");
 
-  });
+/* Create a button (_Fetch all_ in the image above) which when clicked, 
+will trigger a function that will handle the request. 
+The function will return a JSON object with all the characters. => already in the routes.js */
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+fetchAllBtn.addEventListener("click", fetchAllCharacters);
 
-  });
+async function fetchAllCharacters() {
+  displayAllCharacters.innerHTML = "";
+  try {
+    // Get the data and show the characters.
+    const { data } = await axios.get(`${myUrl}characters`);
+    for (const character of data) {
+      createCharacter(character);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
-  });
+//Finally, with JavaScript, we will create a structure similar to a card (image above) to show detailed info about each character.
+function createCharacter(element) {
+  const clone = characterTemplate.content.cloneNode(true);
+  clone.querySelector(".character-id span").textContent = element._id;
+  clone.querySelector(".name span").textContent = element.name;
+  clone.querySelector(".occupation span").textContent = element.occupation;
+  clone.querySelector(".cartoon span").textContent = element.cartoon;
+  clone.querySelector(".weapon span").textContent = element.weapon;
+  displayAllCharacters.append(clone);
+}
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+const fetchOneCharacter = document
+  .getElementById("fetch-one")
+  .addEventListener("click", function (event) {});
 
-  });
+const deleteOneCharacter = document
+  .getElementById("delete-one")
+  .addEventListener("click", function (event) {});
 
+const editCharacter = document
+  .getElementById("edit-character-form")
+  .addEventListener("submit", function (event) {});
+
+const createNewCharacter = document
+  .getElementById("new-character-form")
+  .addEventListener("submit", function (event) {});
